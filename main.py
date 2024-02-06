5fb98cc6f64fa9ee3d86003d8f68429795e0156b
Klemen Berkovic <klemen@atom.feri.um.si>
2024-02-06 12:59:30 -0600

    Added some new fixes

--- main.py
+++ main.py
@@ -16,30 +16,10 @@ from niapy.algorithms.analysis import (
     ExtendedDifferentialGrouping
 )
 from niapy.algorithms.basic import (
-    BareBonesFireworksAlgorithm,
-    ParticleSwarmAlgorithm, 
-    SineCosineAlgorithm,
-    FireflyAlgorithm,
-    HarmonySearch,
-    BatAlgorithm
+    ParticleSwarmAlgorithm
 )
 
 from ccalgorithm import CooperativeCoevolution
-
-
-def run_test_func(no_fun:int, max_evals:int = 3e6) -> None:
-    if 1 < no_fun > 15: raise Exception('Function between 1 and 15!!!')
-    bench = Benchmark()
-    info = bench.get_info(no_fun)
-    fun_fitness = bench.get_function(no_fun)
-    start = timeit.default_timer()
-    i = 0
-    while i < max_evals:
-        sol = info['lower'] + rand(info['dimension']) * (info['upper'] - info['lower'])
-        fun_fitness(sol)
-        i += 1
-    end = timeit.default_timer()
-    print('Time of execution for f%d for %d evals = %fs' % (no_fun, max_evals, end - start))
 
 
 class CEC2013lsgoTask(Task):
@@ -88,35 +68,6 @@ def run_algo(id:int, talgo:type, no_fun:int, *args:list, **kwargs:dict) -> None:
         f1, f2, f3 = task.get_mesures()
         csvfile.write('%d, %f, %f, %f, %f\n' % (id, f1, f2, f3, stop - start))
 
-
-def run_algo_50(talgo:type, no_fun:int, seed:int, *args:list, **kwargs:dict) -> None:
-    for i in range(50): run_algo(seed + i, talgo, no_fun)
-        
-
-def run_fa_cec2013(NP:int = 100, seed:int = 1, no_fun:int = 1) -> None:
-    run_algo_50(FireflyAlgorithm, no_fun, 1, population_size=NP)
-
-
-def run_hs_cec2013(NP:int = 100, seed:int = 1, no_fun:int = 1) -> None:
-    run_algo_50(HarmonySearch, no_fun, 1, population_size=NP)
-
-
-def run_ba_cec2013(NP:int = 100, seed:int = 1, no_fun:int = 1) -> None:
-    run_algo_50(BatAlgorithm, no_fun, 1, population_size=NP)
-
-
-def run_pso_cec2013(NP:int = 100, seed:int = 1, no_fun:int = 1) -> None:
-    run_algo_50(ParticleSwarmAlgorithm, no_fun, 1, population_size=NP)
-
-
-def run_sca_cec2013(NP:int = 100, seed:int = 1, no_fun:int = 1) -> None:
-    run_algo_50(SineCosineAlgorithm, no_fun, 1, population_size=NP)
-
-
-def run_bbfwa_cec2013(NP:int = 100, seed:int = 1, no_fun:int = 1) -> None:
-    run_algo_50(BareBonesFireworksAlgorithm, no_fun, 1, population_size=NP)
-
-
 def no_seps(a:list) -> int:
     s = 0
     for e in a:
@@ -148,17 +99,8 @@ def run_rdg_cec2013(no_fun:int = 1, seed:int = 1, alpha:float = 1e-12, NP:int = 
 
 
 def run_cc_cec2013(no_fun:int = 1, seed:int = 1) -> None:
-    # BareBonesFireworksAlgorithm
-    #algo = CooperativeCoevolution(RecursiveDifferentialGroupingV3(seed=seed), BareBonesFireworksAlgorithm, seed=seed)
-    #algo.set_decomposer_parameters(n=50, alpha=1e-12, tn=50)
-    # SineCosineAlgorithm
-    #algo = CooperativeCoevolution(RecursiveDifferentialGroupingV3(seed=seed), SineCosineAlgorithm, population_size=150, seed=seed)
-    #algo.set_decomposer_parameters(n=50, alpha=1e-12, tn=50)
     # ParticleSwarmAlgorithm
-    #algo = CooperativeCoevolution(RecursiveDifferentialGroupingV3(seed=seed), ParticleSwarmAlgorithm, seed=seed)
-    #algo.set_decomposer_parameters(n=50, alpha=1e-12, tn=50)
-    # FireflyAlgorithm
-    algo = CooperativeCoevolution(RecursiveDifferentialGroupingV3(seed=seed), FireflyAlgorithm, seed=seed)
+    algo = CooperativeCoevolution(RecursiveDifferentialGroupingV3(seed=seed), ParticleSwarmAlgorithm, seed=seed)
     algo.set_decomposer_parameters(n=50, alpha=1e-12, tn=50)
     # create a test cec2013lsgo
     task = CEC2013lsgoTask(no_fun=no_fun)
@@ -180,8 +122,5 @@ if __name__ == "__main__":
     arg_no_fun = int(sys.argv[1]) if len(sys.argv) > 1 else 1
     arg_seed = int(sys.argv[2]) if len(sys.argv) > 2 else 1
     np.set_printoptions(linewidth=np.inf)
-    #run_test_func(no_fun=arg_no_fun)
-    #run_rdg_cec2013(no_fun=arg_no_fun, seed=arg_seed)
-    #run_bbfwa_cec2013(no_fun=arg_no_fun)
     run_cc_cec2013(no_fun=arg_no_fun, seed=arg_seed)
 
